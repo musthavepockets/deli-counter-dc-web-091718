1a552b4c7b9bfc756071e2f850f93f976ad57417
cd <coraldarling@outlook.com>
2018-09-03 18:42:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     deli_line = katz_deli.collect.with_index { |name, index| puts #{index +1}. #{name} }
-    end
     puts "The line is currently: #{deli_line.to_s}"
   end  
 end  
