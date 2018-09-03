49efbf77b13518496a27eeb9cdff2eae2afb6203
cd <coraldarling@outlook.com>
2018-09-03 19:26:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0 
-    "There is no one currently waiting to be served"
+    puts "There is nobody waiting to be served"
   else
-    "Currently serving #{katz_deli.shift}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end  
